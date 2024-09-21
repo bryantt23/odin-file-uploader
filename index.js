736acaaeaa0667bfc843c7c14f4aee4cba378bd9
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-09-22 04:11:02 +0800

    use prisma session store library
    https://github.com/kleydon/prisma-session-store#readme

---
+++ index.js
@@ -0,0 +1,25 @@
+const expressSession = require('express-session')
+const { PrismaSessionStore } = require('@quixo3/prisma-session-store')
+const { PrismaClient } = require('@prisma/client')
+const express = require('express')
+
+const app = express()
+
+app.use(
+    expressSession({
+        cookie: {
+            maxAge: 7 * 24 * 60 * 60 * 1000 // ms
+        },
+        secret: 'a santa at nasa',
+        resave: true,
+        saveUninitialized: true,
+        store: new PrismaSessionStore(
+            new PrismaClient(),
+            {
+                checkPeriod: 2 * 60 * 1000,  //ms
+                dbRecordIdIsSessionId: true,
+                dbRecordIdFunction: undefined
+            }
+        )
+    })
+)
