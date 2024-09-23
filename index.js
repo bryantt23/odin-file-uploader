0f2daf5159a4d089bae09a1fa1e275160703c1e8
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-09-24 01:33:35 +0800

    add crud directory functionality

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const multer = require('multer')
 const upload = multer({ dest: 'uploads/' })
 const path = require('path')
 const PORT = 3000
+const fs = require('fs').promises
 
 const app = express()
 
@@ -41,3 +42,45 @@ app.listen(PORT, function (err) {
     if (err) console.log(err);
     console.log("Server listening on PORT", PORT);
 })
+
+const printDirectories = async source => {
+    console.log("print folders")
+    const directories = (await fs.readdir(source, { withFileTypes: true }))
+        .filter(dirent => dirent.isDirectory())
+        .map(dirent => dirent.name)
+
+    console.log("directories: ", directories)
+}
+
+const makeDirectory = async (path) => {
+    try {
+        await fs.mkdir(path)
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+const renameDirectory = (prev, updated) => {
+    fs.rename(prev, updated)
+}
+
+const deleteDirectory = (path) => {
+    fs.rmdir(path)
+}
+
+const fsTest = async () => {
+    await printDirectories("./uploads")
+    console.log("create a folder")
+    const path = './uploads/test_folder'
+    makeDirectory(path)
+    await printDirectories("./uploads")
+    console.log('rename directory')
+    const renamed = './uploads/test_folder_renamed'
+    renameDirectory(path, renamed)
+    await printDirectories("./uploads")
+    console.log('delete directory')
+    deleteDirectory(renamed)
+    await printDirectories("./uploads")
+}
+
+fsTest()
